a59bd6011a3dfb2616d9b16540943ae2f958d617
Hesam <hesam_mm@live.com>
2025-02-01 04:55:07 -0500

    adding cart and product functionality

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,23 +1,36 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-    
+    name: 'cart',
+    initialState: {
+        items: [], // Initialize items as an empty array
+        totalItems: 0,
     },
-    removeItem: (state, action) => {
+    reducers: {
+        addItem: (state, action) => {
+            const { name, image, cost } = action.payload;
+            const existingItem = state.items.find(item => item.name === name);
+            if (existingItem) {
+                existingItem.quantity++;
+            } else {
+                state.items.push({ name, image, cost, quantity: 1 });
+                state.totalItems++;
+            }
+            console.log( state.items );
+        },
+        removeItem: (state, action) => {
+            state.items = state.items.filter(item => item.name !== action.payload);
+            state.totalItems--;
+        },
+        updateQuantity: (state, action) => {
+            const { name, quantity } = action.payload;
+            const itemToUpdate = state.items.find(item => item.name === name);
+            if (itemToUpdate) {
+                itemToUpdate.quantity = quantity;
+            }
+        },
     },
-    updateQuantity: (state, action) => {
-
-    
-    },
-  },
 });
-
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
 export default CartSlice.reducer;
